0ab318dd588b8d362ad73a2e461784106a3e04e4
WMXZ-EU <WMXZ-EU@users.noreply.github.com>
2020-11-25 20:07:41 +0100

    Update MTP.cpp

--- src/MTP.cpp
+++ src/MTP.cpp
@@ -32,9 +32,8 @@
 
 #if defined(__IMXRT1062__)
   // following only while usb_mtp is not included in cores
-  #if __has_include("usb_mtp.h")
-//    #include "usb_mtp.h"
-    #include "usb1_mtp.h"
+  #if __has_include("usb_mtp.h")  && WMXZ_TEST
+    #include "usb_mtp.h"
   #else
     #include "usb1_mtp.h"
   #endif
